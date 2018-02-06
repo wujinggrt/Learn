802981ebb13490e6f38a2aed2f3a92e87d2a4e3e
Victor <1094514854@qq.com>
2018-02-07 07:09:42 +0800

    Update

---
+++ Java/logger/LoggerTest.java
@@ -0,0 +1,13 @@
+package logger;
+
+import java.util.logging.Logger;
+
+public class LoggerTest {
+    public static void main(String[] args) {
+        Logger.getGlobal().info("Test: Line5");
+        Logger.getGlobal().info("Test: Line6");
+        Logger.getGlobal().info("Test: Line7");
+        Logger.getGlobal().info("Test: Line8");
+        System.out.println("Hello");
+    }
+}
