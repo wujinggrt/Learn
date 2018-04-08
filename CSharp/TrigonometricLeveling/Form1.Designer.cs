b89478fc272dd7f07de55c48bdaff9b8812037cf
wujinggrt <wujinggrt@qq.com>
2018-04-08 22:32:25 +0800

    Update VS project TrigonometricLeveling

---
+++ CSharp/TrigonometricLeveling/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace TrigonometricLeveling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.addButton = new System.Windows.Forms.Button();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column5,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column6});
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridView1.Location = new System.Drawing.Point(34, 104);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(825, 310);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyDown);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "起始点高程(m)";
+            this.Column1.Name = "Column1";
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column5.HeaderText = "竖直角(角度制)";
+            this.Column5.Name = "Column5";
+            this.Column5.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "两点水平距离(m)";
+            this.Column2.Name = "Column2";
+            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.HeaderText = "仪器高(m)";
+            this.Column3.Name = "Column3";
+            this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column4.HeaderText = "目标高(m)";
+            this.Column4.Name = "Column4";
+            this.Column4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column6
+            // 
+            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column6.HeaderText = "目标点高程(m)";
+            this.Column6.Name = "Column6";
+            this.Column6.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(34, 36);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(129, 42);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "添加(&A)";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(188, 36);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(129, 42);
+            this.calculateButton.TabIndex = 2;
+            this.calculateButton.Text = "计算选中行(&C)";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(346, 36);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(129, 42);
+            this.deleteButton.TabIndex = 3;
+            this.deleteButton.Text = "删除选中行(&D)";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(507, 36);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(129, 42);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "清空(&E)";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 497);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "三角高程测量";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ CSharp/TrigonometricLeveling/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrigonometricLeveling
+{
+    public partial class Form1 : Form
+    {
+        private List<bool> columnStatus = new List<bool>(5);
+
+        public Form1()
+        {
+            InitializeComponent();
+            AddRow();
+            SetFormat();
+        }
+
+        private void SetFormat()
+        {
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.ReadOnly = false;
+        }
+
+        private int AddRow()
+        {
+            int y = dataGridView1.Rows.Add();
+            dataGridView1.Rows[y].Cells[5].ReadOnly = true;
+            return y;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddRow();
+        }
+
+        // enter事件
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                bool status = InputCheck();
+                if (status)
+                {
+                    Compute();
+                    dataGridView1.CurrentRow.ReadOnly = true;
+                    // 选中新建行的行首
+                    int y = AddRow();
+                    dataGridView1.CurrentCell = dataGridView1[0, y];
+                }
+                e.Handled = true;
+            }
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            bool status = InputCheck();
+            if (status)
+            {
+                Compute();
+                dataGridView1.CurrentRow.ReadOnly = true;
+                // 选中新建行的行首
+                int y = AddRow();
+                dataGridView1.CurrentCell = dataGridView1[0, y];
+            }   
+        }
+
+        private bool InputCheck()
+        {
+            columnStatus.Clear();
+            for(int i = 0; i < dataGridView1.ColumnCount - 1; ++i)
+            {
+                // 空的情况
+                if (dataGridView1.CurrentRow.Cells[i].Value == null)
+                {
+                    columnStatus.Add(false);
+                    continue;
+                }
+                double temp = 0.0;
+                string data = dataGridView1.CurrentRow.Cells[i].Value.ToString();
+                if (!double.TryParse(data, out temp))
+                {
+                    columnStatus.Add(false);
+                    continue;
+                }
+                else
+                {
+                    columnStatus.Add(true);
+                    continue;
+                }
+            }
+            string indexStr = "";
+            bool allStatus = true;
+            for (int i = 0; i < columnStatus.Count; ++i)
+            {
+                if (!columnStatus[i])
+                {
+                    allStatus = false;
+                    indexStr += i + 1 + " ";
+                }
+            }
+            if (!allStatus)
+            {
+                MessageBox.Show("以下格子数据错误！\n" + indexStr, "错误", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            return true;
+        }
+
+        private void Compute()
+        {
+            var currentRow = dataGridView1.CurrentRow;
+            double beginHeight = double.Parse(
+                currentRow.Cells[0].Value.ToString());
+            double verticalAngle = double.Parse(
+                currentRow.Cells[1].Value.ToString());
+            double distance = double.Parse(
+                currentRow.Cells[2].Value.ToString());
+            double instrumentHeight = double.Parse(
+                currentRow.Cells[3].Value.ToString());
+            double targetHeight = double.Parse(
+                currentRow.Cells[4].Value.ToString());
+            double endHeight =
+                beginHeight +
+                distance * Math.Tan(RadianTransform(verticalAngle)) +
+                instrumentHeight -
+                targetHeight;
+            currentRow.Cells[5].Value = endHeight.ToString("f3");
+        }
+
+        private double RadianTransform(double angle)
+        {
+            double degree = (int)angle;
+            double minute = (int)((angle - degree) * 100);
+            double second = (int)((angle - degree) * 100 - minute) *100;
+            double ret = (degree + minute / 60 + second / 3600) / 180 * Math.PI;
+            return ret;
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if(dataGridView1.CurrentRow != null)
+            {
+                dataGridView1.Rows.Remove((dataGridView1.CurrentRow));
+            }
+            // 当行为空的时候，自动添加一行
+            if(dataGridView1.RowCount == 0)
+            {
+                AddRow();
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            AddRow();
+        }
+    }
+}
