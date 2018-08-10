38ba4a205e40195411c82689d4237169c0b22cb2
wujing <wujinggrt@qq.com>
2018-08-10 23:10:13 +0800

    update

---
+++ Java/Maven/helloworld/src/main/java/com/wujing/App.java
@@ -0,0 +1,13 @@
+package com.wujing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
