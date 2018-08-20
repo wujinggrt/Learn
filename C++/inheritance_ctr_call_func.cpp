0a797430c6b72eb04961261a0c889bede924dbd5
wujinggrt <wujinggrt@qq.com>
2018-08-21 02:24:04 +0800

    update

--- C++/inheritance_ctr_call_func.cpp
+++ C++/inheritance_ctr_call_func.cpp
@@ -3,25 +3,32 @@ using namespace std;
 
 class Base1 {
 public:
-	Base1() {
+	Base1() :_i(f()) {
+		f();
+	}
+
+	Base1(int i) : _i(i) {
+		cout << _i << endl;
 		f();
 	}
 	
-	virtual void f() {
-		cout << "Base1" << endl;
+	virtual int f() {
+		cout << "Base1" << 10 << endl;
+		return 10;
 	}
+
+	int _i;
 };
 
 class Base2 : public virtual Base1 {
 public:
-	// 错误的，下一行代码，因为vptr还没有初始化
-	// Base2():f() {
-	Base2():f() {
+	Base2() : Base1(f()) {
 		f();
 	}
 
-	void f() {
-		cout << "Base2" << endl;
+	int f() {
+		cout << "Base2" << 100 << endl;
+		return 100;
 	}
 };
 
@@ -29,6 +36,6 @@ int main() {
 	Base1 b1;
 	Base2 b2;
 	
-	b2.f();
+//	b2.f();
 	return 0;
 }
