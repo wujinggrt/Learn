4eb38b359e89caff3ac16a82b327249054a0977a
wujinggrt <wujinggrt@qq.com>
2018-08-21 22:15:41 +0800

    update

--- C++/model/plain_ol.cpp
+++ C++/model/plain_ol.cpp
@@ -19,6 +19,20 @@ int main() {
 	print_point(global);
 	cout << "local:" << endl;
 	print_point(local);
+
+/*
+全局变量是初始化的，因为有__sti（对于存在default constructor而言）等因素
+输出：
+global:
+0
+0
+0
+local:
+-5.85354e+20
+4.59163e-41
+0
+*/
+
 	return 0;
 }
 
