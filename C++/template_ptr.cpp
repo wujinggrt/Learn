12cad90bd829dba7adbeecdbce53f2da1b595581
wujinggrt <wujinggrt@qq.com>
2018-08-26 00:35:30 +0800

    update

--- C++/template_ptr.cpp
+++ C++/template_ptr.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 template<typename T>
@@ -7,10 +8,28 @@ void f(T p)
     cout << *p << endl;
 }
 
+class A
+{
+public:
+    A(string s = "Hello")
+        : _s(new string(s)) {}
+
+    string& operator*() const { return *_s; }
+    string* operator->() const { return _s; }
+
+private:
+    string *_s;
+};
+
 int main()
 {
     int *p = new int(5);
     f(p);
 // 5
+
+    A a(string("hehe"));
+    cout << "*a: " << *a << endl;
+    cout << "a->size(): " << a->size() << endl;
+
     return 0;
 }
